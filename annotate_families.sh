716d6ba5286ddc9964cce0e52e939d6f6c757743
Athanasios Teodosiadis <nasi4@altiusinstitute.org>
2019-12-02 01:46:39 -0500

    Update annotate_families.sh

--- annotate_families.sh
+++ annotate_families.sh
@@ -6,13 +6,6 @@
 #DNA -> hAT-Charlie, TcMar-Tigger, Others
 #LINE -> L1, L2, Others
 
-mkdir -p /home/nasi4/proj/encode3/DHS_Annotations/Repeated_Regions/results_streamline/families
-wdir=/home/nasi4/proj/encode3/DHS_Annotations/Repeated_Regions/results_streamline/families
-cd $wdir
-
-file=/home/nasi4/proj/encode3/DHS_Annotations/Repeated_Regions/results_streamline/dhs_annotated_7-repeats.bed
-
-
 #Split $file into the four classes that have familes
 for i in SINE LINE LTR DNA
 do
@@ -28,4 +21,39 @@ awk '{if($5 != "Alu" &&  $5 != "MIR") {
         print $1"\t"$2"\t"$3"\t"$4"\t"$5;
         }
 }' tmp.SINE.bed > SINE.bed
+
+#LTR -> ERVL-MaLR, ERV1, ERVL, Others
+awk '{if($5 != "ERVL-MaLR" &&  $5 != "ERV1" && $5 != "ERVL") {
+        print $1"\t"$2"\t"$3"\t"$4"\t""Others"; 
+        }
+        
+        else {
+        print $1"\t"$2"\t"$3"\t"$4"\t"$5;
+        }
+}' tmp.LTR.bed > LTR.bed
+
+#DNA -> hAT-Charlie, TcMar-Tigger, Others
+awk '{if($5 != "hAT-Charlie" &&  $5 != "TcMar-Tigger") {
+        print $1"\t"$2"\t"$3"\t"$4"\t""Others"; 
+        }
+        
+        else {
+        print $1"\t"$2"\t"$3"\t"$4"\t"$5;
+        }
+}' tmp.DNA.bed > DNA.bed
+
+#LINE -> L1, L2, Others
+awk '{if($5 != "L1" &&  $5 != "L2") {
+        print $1"\t"$2"\t"$3"\t"$4"\t""Others"; 
+        }
+        
+        else {
+        print $1"\t"$2"\t"$3"\t"$4"\t"$5;
+        }
+}' tmp.LINE.bed > LINE.bed
+
+
+
+#Clean Up
+rm tmp.*
 
